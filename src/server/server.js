97c8a99121e7264e81075ef0f123fdc0bf101e4b
scrumvisualize <scrumvisualize@gmail.com>
2023-08-11 23:28:57 +1000

    remove comments and url in package.json

--- src/server/server.js
+++ src/server/server.js
@@ -15,23 +15,15 @@ const godaddyUserEmail = process.env.GODADDY_USER_EMAIL;
 const godaddyEmailPassword = process.env.GODADDY_EMAIL_PASSWORD;
 const recipientJegniLifeEmail = process.env.JEGNILIFE_RECIPIENT_EMAIL;
 
-//Allow request from specific origin:
-
-// const corsOptions = {
-//   origin : 'https://www.jegnilife.com.au',
-//   credentials: true,   // access-control-allow-credentials:true
-//   optionSuccessStatus : 200
-// }
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
   origin: 'https://www.jegnilife.com.au',
-  methods: 'GET, POST, PUT, OPTIONS',
+  methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Authorization, Content-Type',
-  credentials: true,
   exposedHeaders: 'Content-Length',
+  credentials: true,
   maxAge: 3600
 };
 
@@ -40,25 +32,10 @@ app.use(cors(corsOptions)) // Use this after the variable declaration
 // Set up a route to handle the email sending
 app.post('/service/emailtransfer', async (req, res) => {
 
-  // res.header('Access-Control-Allow-Origin', 'https://jegnilife.com.au');
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.header('Access-Control-Allow-Headers', 'Athorization, Content-Type');
-  // res.header('Access-Control-Expose-Headers', 'Content-Length');
-  // res.header('Access-Control-Max-Age', '3600');
-
   const senderEmail = req.body.email;
   const senderMobile = req.body.mobile;
   const senderComments = req.body.comments;
   console.log("Contact details in server side:: "+senderEmail, senderMobile, senderComments );
-
-  // Create a nodemailer transporter
-  // const transporter = nodemailer.createTransport({
-  //   service: 'Gmail', // Replace with your email service provider
-  //   auth: {
-  //     user: gmailUserEmail, // Replace with your email
-  //     pass: gmailAppPassword, // Replace with your app password
-  //   },
-  // });
 
   const transporter = nodemailer.createTransport({
     //service: 'smtpout.secureserver.net', // Replace with your godaddy email service provider
@@ -76,7 +53,6 @@ app.post('/service/emailtransfer', async (req, res) => {
       pass: godaddyEmailPassword, // Replace with your godaddy email password
     },
   });
-
 
   try {
     await transporter.sendMail({
