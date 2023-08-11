12b75cccf4c749829ebc84f3ae80975d6b0042ec
scrumvisualize <scrumvisualize@gmail.com>
2023-08-11 15:52:51 +1000

    Adding headers

--- src/server/server.js
+++ src/server/server.js
@@ -17,11 +17,20 @@ const recipientJegniLifeEmail = process.env.JEGNILIFE_RECIPIENT_EMAIL;
 
 //Allow request from specific origin:
 
+// const corsOptions = {
+//   origin : 'https://www.jegnilife.com.au',
+//   credentials: true,   // access-control-allow-credentials:true
+//   optionSuccessStatus : 200
+// }
+
 const corsOptions = {
-  origin : 'https://www.jegnilife.com.au',
-  credentials: true,   // access-control-allow-credentials:true
-  optionSuccessStatus : 200
-}
+  origin: 'https://www.jegnilife.com.au',
+  methods: 'GET, POST, PUT, OPTIONS',
+  allowedHeaders: 'Authorization, Content-Type',
+  credentials: true,
+  exposedHeaders: 'Content-Length',
+  maxAge: 3600
+};
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
