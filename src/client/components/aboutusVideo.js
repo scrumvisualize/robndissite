6d2bacc5c0885625d9f0f27e071167d84f697bc8
scrumvisualize <scrumvisualize@gmail.com>
2023-08-24 14:14:35 +1000

    css changes and video heading

--- src/client/components/aboutusVideo.js
+++ src/client/components/aboutusVideo.js
@@ -11,10 +11,14 @@ const AboutusVideo = () => {
 
     return (
         <div className="flex-wrap relative">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center md:px-5 py-2 md:py-8 bg-sky-50 text-gray-600">
+                JEGNI LIFE's service highlights
+            </h1>
             <div className="flex flex-col md:flex-row bg-sky-50">
                 <div className="md:w-1/1 p-4 rounded-lg overflow-hidden relative mx-auto ">
                     <div className="flex justify-center items-center mx-auto ml-0 w-[348px] md:w-[805px] h-[250px] md:h-[400px] rounded-lg relative">
                         <iframe
+                            className="rounded-lg"
                             width="100%"
                             height="100%"
                             src="https://www.youtube.com/embed/4HkEC43t6zo"
--- src/client/components/makesEasier.js
+++ src/client/components/makesEasier.js
@@ -19,12 +19,12 @@ const MakesEasier = () => {
 
     return (
         <div className="relative">
-            <div className="w-full md:h-[650px] md:mt-0 bg-no-repeat bg-cover bg-life-easier">
+            <div className="w-full h-[280px] md:h-[650px] md:mt-0 bg-no-repeat bg-cover bg-life-easier">
                 <div className="relative inset-0 flex flex-col justify-center items-center text-white">
-                    <h1 className="text-2xl md:text-2xl lg:text-4xl font-bold mt-4 md:mt-40 ml-50">We make your life easier</h1>
+                    <h1 className="text-2xl md:text-2xl lg:text-4xl font-bold mt-6 md:mt-40 ml-50">We make your life easier</h1>
                     <p className="text-sm md:text-lg md:text-base lg:text-lg ml-0"> Our diligent services make your life</p>
                     <p className="text-sm md:text-lg md:text-base lg:text-lg ml-00"> much easier and happier!</p>
-                    <button onClick={performDownload} className="flex justify-center items-center mx-4 mb-4 md:mx-24 md:mb-20 mx-24 mb-20 md:mt-20 select-none bg-sky-400 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded-full" download target='_blank'>
+                    <button onClick={performDownload} className="flex justify-center items-center mx-4 mb-4 md:mx-24 md:mb-20 mx-24 mb-20 mt-12 md:mt-20 select-none bg-sky-400 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded-full" download target='_blank'>
                             Download info pack
                             <svg className="w-4 h-4 text-slate-50 mx-3 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
