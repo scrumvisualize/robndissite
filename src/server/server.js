e38fff0a67931062459d39cec63f65ad2ef92309
scrumvisualize <scrumvisualize@gmail.com>
2024-05-17 12:02:56 +1000

    Updated mailersend api key

--- src/server/server.js
+++ src/server/server.js
@@ -2,23 +2,33 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const path = require('path')
+const path = require('path');
 const dotenv = require('dotenv');
-const sgMail = require('@sendgrid/mail')
+const axios = require('axios');
+const { MailerSend, EmailParams, Sender, Recipient } = require('mailersend');
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const app = express();
 
-// const port = 8000;
-
 const port = process.env.PORT || 8000;
 const godaddyUserEmail = process.env.GODADDY_USER_EMAIL;
 const godaddyEmailPassword = process.env.GODADDY_EMAIL_PASSWORD;
 const recipientJegniLifeEmail = process.env.JEGNILIFE_RECIPIENT_EMAIL;
-const sendGridToken = process.env.SENDGRID_MAIL_KEY;
-const sendGridToEmail = process.env.SENDGRID_TO_EMAIL_ADDRESS;
-const sendGridFromEmail = process.env.SENDGRID_FROM_EMAIL_ADDRESS;
+const mailersendToEmail = process.env.MAILERSEND_TO_EMAIL_ADDRESS;
+
+/* Getting mailerSend api key from the .env file */ 
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY,
+});
+
+/* MailerSend need a sender email details and name */ 
+const sentFrom = new Sender("info@jegnilife.com.au", "jegnilife");
+/* MailerSend need a valid To email address to send and name */ 
+const recipients = [
+  new Recipient(`${mailersendToEmail}`, "Jegnilife")
+];
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,35 +39,34 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-sgMail.setApiKey(sendGridToken);
-
-// Set up a route to handle the email sending
+// Set up a server side service/route to handle the email sending using mailersend api
 app.post('/service/emailtransfer', async (req, res) => {
 
   const senderEmail = req.body.email;
   const senderMobile = req.body.mobile;
   const senderComments = req.body.comments;
-  console.log("Contact details in server side:: " + senderEmail, senderMobile, senderComments);
-
-  const to = sendGridToEmail;
-  const from = sendGridFromEmail;
-  const subject = "Email enquiry request from JEGNI Life website";
-  const mailhtml = `<div><p>Senders Email: ${senderEmail}</p><p>Mobile Number: ${senderMobile}</p><p>Comments: ${senderComments}</p><img src="https://jegnilife.com.au/images/emailEnquiry.png" alt="community" width="340" height="453"></img></div>`;
+  
   try {
-    const msg = {
-      to,
-      from,
-      subject,
-      html: mailhtml
-    }
-    await sgMail.send(msg);
-    return res.status(200).json({ success: true });
-  } catch (e) {
-    console.log(e);
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setSubject("New enquiry from Jegnilife website")
+      .setHtml(`<div><p>Requesting persons email address: ${senderEmail}</p><p>Contact Number: ${senderMobile}</p><p>Comments: ${senderComments}</p><img src="https://jegnilife.com.au/images/emailEnquiry.png" alt="community" width="340" height="453"></img></div>`)
+      .setText(`${senderComments}`);
+
+    await mailerSend.email.send(emailParams);
+
+    // If the email sending is successful, send a response to the client
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    // If an error occurs during email sending, handle it here
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
+
 });
 
 app.listen(port, () => {
